29f78fb55a68ba90721329062d4353010efc83df
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-02-04 00:25:51 -0300

    GetBasic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
  import express from 'express';
  const app = express(); 
 
+ const livros = [
+   {
+      id: 1,
+      titulo: 'O Senhor dos Anéis',
+      autor: 'J.R.R. Tolkien'
+   },
+   {
+      id: 2,
+      titulo: 'A Game of Thrones',
+      autor: 'George R. R. Martin'
+   }
+ ]
  app.get('/', (req, res) =>{
     res.status(200).send('Hello World')
  });
+ app.get('/listLivros', (req, res) =>{
+   res.status(200).json(livros)
+});
  
  export default app;
