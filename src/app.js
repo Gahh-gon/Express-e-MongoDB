eadc662c26da50ea04a8051cf5f0f19cf86c7428
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-02-04 23:08:39 -0300

    PostBasic

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
  import express from 'express';
  const app = express(); 
-
+app.use(express.json());
  const livros = [
    {
       id: 1,
@@ -20,4 +20,8 @@
    res.status(200).json(livros)
 });
  
+app.post('/listlivros', (req, res) =>{
+livros.push(req.body);
+res.status(201).send("livro cadastrado com sucesso")
+});
  export default app;
