4ac9a8be9ab20181e486cf0b1378e6708f68fba5
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-02-07 02:29:50 -0300

    searchEPutBasic

--- src/app.js
+++ src/app.js
@@ -13,15 +13,29 @@ app.use(express.json());
       autor: 'George R. R. Martin'
    }
  ]
+ function buscarLivroId(id){
+   return livros.findIndex(livros =>{
+     return livros.id === Number(id);
+   })
+ }
  app.get('/', (req, res) =>{
     res.status(200).send('Hello World')
  });
  app.get('/listLivros', (req, res) =>{
    res.status(200).json(livros)
 });
- 
+app.get('/Livro/:id', (req, res) =>{
+   const id = buscarLivroId(req.params.id);
+   res.status(200).json(livros[id])
+});
 app.post('/listlivros', (req, res) =>{
 livros.push(req.body);
 res.status(201).send("livro cadastrado com sucesso")
+});
+
+app.put('/listlivros/:id', (req, res) =>{
+   const id = buscarLivroId(req.params.id);
+   livros[id].titulo = req.body.titulo;
+   res.status(200).json(livros[id])
 });
  export default app;
