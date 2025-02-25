b7ca2d65d551c4fe2e4657b956185145e6ea4bdf
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-02-24 21:50:19 -0300

    ConnectMongoeMongoose

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
  import express from 'express';
+ import connectDataBase from './config/dbConnect.js';
+
+ const conexao = await connectDataBase();
+  
+ conexao.on("error",(erro) =>{
+   console.error("Erro de conexão com o banco de dados", erro);
+  
+ })
+ conexao.once("open",() =>{
+   console.log("Conexão com o banco de dados estabelecida com sucesso");  // imprime no console a mensagem quando o server está rodando
+ })
  const app = express(); 
 app.use(express.json());
  const livros = [
