d15224d6bd4275eb8ebb957164c27783c35f38f2
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-03-20 20:01:56 -0300

    ControPostRoutesWorks
    Criei uma rota POST no controller e ajustei as configurações de roteamento para garantir que funcionem corretamente.

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,7 +4,18 @@ class LivroController {
 static async listarLivro(req,res){
     const listaLivros = await livro.find({});
     res.status(200).json(listaLivros)
+};
+static async cadastrarLivro (req, res) { 
+    livro.push(req.body);
+   
+    try{
+        const novoLivro = await livro.create(req.body)
+        res.status(201).json({message:"criando com sucesso", livro:novoLivro})
+    }catch(erro){
+        res.status(500).json({message:`${erro.message} - falha ao cadastrar livro`})
+    }
 }
+
 }
 
 export default LivroController; 
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,3 +4,6 @@ import LivroController from '../controllers/livroController.js';
 const routes = express.Router();
 
 routes.get('/listLivros', LivroController.listarLivro);
+routes.post('/listlivros', LivroController.cadastrarLivro)
+
+export default routes;
