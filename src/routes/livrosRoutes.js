dd35c64395e9218e1ebd79d885f5a1f7058325a7
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-03-22 00:16:18 -0300

    CrudBasicDone
    Refatoração finalizada no DELETE, CRUD básico completo e funcional

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,8 +4,9 @@ import LivroController from '../controllers/livroController.js';
 const routes = express.Router();
 
 routes.get('/listLivros', LivroController.listarLivros);
-routes.get('/listLivros:id', LivroController.listarLivroPorId);
-routes.put('/listLivros:id', LivroController.AtualizarLivro)
+routes.get('/listLivros/:id', LivroController.listarLivroPorId);
+routes.put('/listLivros/:id', LivroController.AtualizarLivro)
 routes.post('/listlivros', LivroController.cadastrarLivro)
+routes.delete('/listlivros/:id', LivroController.deletarLivro)
 
 export default routes;
