5f5c3b7a6465b34264d156ce1b06216a53c9d41e
Gahh-gon <gabriel000.g.g.p@gmail.com>
2025-03-21 23:07:38 -0300

    controllerRotaPut
    Refatorada a rota PUT para controllers e rotas, além da refatoração do GET que Busca por ID.

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,9 +1,36 @@
 import livro from "../models/livros.js";
 
 class LivroController {
-static async listarLivro(req,res){
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros)
+static async listarLivros(req,res){
+    try{
+        const listaLivros = await livro.find({});
+        res.status(200).json({message:"Livros Listados com sucesso",livro:listaLivros})
+    }catch(err){
+        res.status(500).json({message:`${err.message} - falha ao listar livros`})
+    }
+    
+};
+static async listarLivroPorId(req,res){
+    try{
+        const id = req.params.id;
+      
+        const LivroEncontr = await livro.findById(id);
+        res.status(200).json({message:"Livro encotrando com sucesso",livro:LivroEncontr})
+    }catch(err){
+        res.status(500).json({message:`${err.message} - falha ao encontrar livro`})
+    }
+    
+};
+static async AtualizarLivro(req,res){
+    try{
+        const id = req.params.id;
+      
+     await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({message:"Livro atualizado",livro:LivroEncontr})
+    }catch(err){
+        res.status(500).json({message:`${err.message} - falha na atualização`})
+    }
+    
 };
 static async cadastrarLivro (req, res) { 
     livro.push(req.body);
